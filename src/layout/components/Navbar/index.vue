5038f06e9906d7e2e9223c513b2e9d90c5569d94
sanjiu <1053835782@qq.com>
2021-06-11 00:35:40 +0800

    add file

---
+++ src/layout/components/Navbar/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="navbar">Navbar</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { AppModule } from "@/store/modules/app";
+import { UserModule } from "@/store/modules/user";
+// import { UserModule } from "@/store/modules/";
+
+@Component({
+  name: "Navbar",
+  components: {},
+})
+export default class extends Vue {
+  get sidebar() {
+    return AppModule.sidebar;
+  }
+
+  get device() {
+    return AppModule.device.toString();
+  }
+
+  get avatar() {
+    return require("@/assets/avatar.gif");
+  }
+
+  private toggleSideBar() {
+    AppModule.ToggleSideBar(false);
+  }
+
+  private async logout(){
+      UserModule.ResetToken()
+  }
+
+  private themeChange(value: string){
+      
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+}
+</style>
